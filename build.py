8569733f631424ec6ffc6c209cfbaae598ebe291
Bastien Sevajol <sevajol.bastien@gmail.com>
2016-04-03 17:38:52 +0200

    service_model.yml generation

--- build.py
+++ build.py
@@ -4,7 +4,8 @@ import argparse
 import os
 import shutil
 import yaml
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, Template
+from slugify import slugify
 
 
 class Described(object):
@@ -38,6 +39,12 @@ class Project(Described):
         # groups = set([group for group in host.groups for host in hosts]) ?
         return groups
 
+    def get_service(self, service_name):
+        for service in self.services:
+            if service['name'] == service_name:
+                return service
+        raise Exception("Service %s not found" % service_name)
+
 
 class Loader(object):
     @staticmethod
@@ -48,9 +55,92 @@ class Loader(object):
             return Project(name, description)
 
 
+class Model(object):
+    name = NotImplemented
+    defaults = {}
+
+    def __init__(self, builder, service):
+        self._builder = builder
+        self._service = service
+
+    @property
+    def service_slug(self):
+        return slugify(self._service['name'])
+
+    @property
+    def service_name(self):
+        return self._service['name']
+
+    def get_parameter(self, parameter_name):
+        return self.get_parameters()[parameter_name]
+
+    def get_parameters(self):
+        parameters = self._service['parameters'][self.name]
+        for default_parameter in self.defaults:
+            if default_parameter not in parameters:
+                parameters[default_parameter] = self.defaults[default_parameter](self)
+        return parameters
+
+    def render(self, **kwargs):
+        template_file_path = "%s/tasks/services/%s.yml" % (self._builder.template_dir, self.name)
+        with open(template_file_path, 'r') as f:
+            template = Template(f.read())
+            kwargs.update(self.get_parameters())
+            kwargs.update({
+                '__service_name__': self.service_name,
+                '__model_name__': self.name
+            })
+            return template.render(**kwargs)
+
+    def get_task(self):
+        return self.render()
+
+
+class LAMPWebsiteModel(Model):
+    name = 'lamp_website'
+    defaults = {
+        'mysql_user_name': lambda model: model.service_slug
+    }
+
+    @property
+    def mysql_user_name(self):
+        return self.service_name
+
+    @property
+    def mysql_password(self):
+        return self.get_parameter('mysql_password')
+
+
+class Models(object):
+    _models = {
+        LAMPWebsiteModel.name: LAMPWebsiteModel,
+        # LAMPWebsiteBackupedModel.name: LAMPWebsiteBackupedModel,
+    }
+
+    @classmethod
+    def get(cls, model_name, service, builder):
+        return cls._models[model_name](builder, service)
+
+
 class Services(object):
-    def __init__(self, project):
-        self._project = project
+    def __init__(self, builder):
+        self._builder = builder
+        self._project = builder.project
+
+    def get_tasks(self):
+        tasks = []
+        for service in self._project.services:
+            service_name = service['name']
+            for model_name in service['models']:
+                task = self.get_task(service_name, model_name)
+                task_name = "%s_%s" % (slugify(service_name), model_name)
+                tasks.append((task_name, task))
+        return tasks
+
+    def get_task(self, service_name, model_name):
+        service = self._project.get_service(service_name)
+        model = Models.get(model_name, service, builder=self._builder)
+        return model.get_task()
 
 
 class Builder(object):
@@ -60,21 +150,30 @@ class Builder(object):
         self._build_dir = "%s/%s" % (output, self._project.name)
         self._templates_dir = templates_dir
 
+    @property
+    def project(self):
+        return self._project
+
+    @property
+    def template_dir(self):
+        return self._templates_dir
+
     def build(self):
         self.build_hosts()
         self.build_project_vars()
+        self.build_groups_tasks()
         services = self.get_services()
-        self.build_hosts_tasks(services)
         self.build_services_tasks(services)
+        self.build_hosts_tasks(services)
 
     def build_hosts(self):
         hosts = self.get_hosts_build(self._project.get_hosts())
         self._output('hosts', hosts)
 
     def get_services(self):
-        return Services(self._project)
+        return Services(self)
 
-    def build_hosts_tasks(self, services):
+    def build_groups_tasks(self):
         from_dir = "%s/tasks/groups" % self._templates_dir
         to_dir = "%s/tasks/groups" % self._build_dir
         group_tasks = self._project.get_hosts_groups()
@@ -90,8 +189,12 @@ class Builder(object):
     def build_project_vars(self):
         self._output("vars.yml", yaml.dump(self._project.vars, default_flow_style=False, indent=4))
 
-    def build_services_tasks(self, services):
+    def build_hosts_tasks(self, services):
         pass
+
+    def build_services_tasks(self, services):
+        for task_name, task in services.get_tasks():
+            self._output("services/%s.yml" % task_name, yaml.dump(task, default_flow_style=False, indent=4))
 
     @staticmethod
     def _copy_files(from_dir, to_dir, files):
@@ -99,16 +202,18 @@ class Builder(object):
             os.makedirs(to_dir)
 
         for file_name in files:
-            from_file_path =  "%s/%s.yml" % (from_dir, file_name)
+            from_file_path = "%s/%s.yml" % (from_dir, file_name)
             if not os.path.exists(from_file_path):
                 raise Exception("File not found: \"%s\"" % from_file_path)
 
             shutil.copyfile(from_file_path, "%s/%s.yml" % (to_dir, file_name))
 
     def _output(self, file_path, content):
-        if not os.path.exists(self._build_dir):
-            os.makedirs(self._build_dir)
-        with open("%s/%s" % (self._build_dir, file_path), "wb") as f:
+        complete_file_path = "%s/%s" % (self._build_dir, file_path)
+        complete_dir_path = '/'.join(complete_file_path.split('/')[0:-1])
+        if not os.path.exists(complete_dir_path):
+            os.makedirs(complete_dir_path)
+        with open(complete_file_path, "wb") as f:
             f.write(content)
 
     def get_hosts_build(self, hosts):
